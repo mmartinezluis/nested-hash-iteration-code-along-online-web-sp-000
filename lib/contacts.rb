923d2180ec604bcd97057b36def18c01d9a85850
Luis Martinez <luis_mmartinez@live.com>
2020-12-29 01:36:39 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,8 +16,12 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |person, general_info|
+
     general_info.each do |attribute, value|
-      value.delete_if {|ice_cream| ice_cream == "Strawberry"}
+
+      if attribute == :favorite_ice_cream_flavors
+        value.delete_if {|ice_cream| ice_cream == "Strawberry"}
+      end
     end
   end
 end
